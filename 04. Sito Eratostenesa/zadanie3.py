7cee3a5846fea5d52b0e1c99bbf16fced16d8a58
fedoseevm <maksim.fedoseev122@gmail.com>
2024-10-02 16:15:51 +0200

    Add zadanie5.py

--- 04. Sito Eratostenesa/zadanie3.py
+++ 04. Sito Eratostenesa/zadanie3.py
@@ -15,3 +15,19 @@ while p * p <= n:
 for i in range(2, n + 1):
     if czy_pierwsza[i]:
         print(i, end=" ")
+
+
+# czy_pierwsza[0] ← fałsz
+# czy_pierwsza[1] ← fałsz
+# dla i=2,3,...,n wykonuj
+#   czy_pierwsza[i] ← prawda
+# p ← 2
+# dopóki p * p ⩽ n wykonuj
+#   jeżeli czy_pierwsza[p]=prawda
+#     index ← p * p
+#     dopóki index ⩽ n wykonuj
+#       czy_pierwsza[i] ← fałsz
+#   p ← p + 1
+# dla i=2,3,...,n wykonuj
+#   jeżeli czy_pierwsza[i]=prawda to
+#     wypisz i
---
+++ 04. Sito Eratostenesa/zadanie5.py
@@ -0,0 +1,17 @@
+a, b = map(int, input("Podaj przedzial (a i b): ").split())
+
+czy_pierwsza = [True] * (b - a + 1)
+print(czy_pierwsza)
+czy_pierwsza[0] = False
+czy_pierwsza[1] = False
+
+p = 2
+while p * p < len(czy_pierwsza):
+    if czy_pierwsza[p]:
+        for i in range(p * p, len(czy_pierwsza), p):
+            czy_pierwsza[i] = False
+    p += 1
+
+for i in range(len(czy_pierwsza)):
+    if czy_pierwsza[i]:
+        print(i + a, end=" ")
