9818e4654862a46a896bd1800dfe8cec8a0c879b
fedoseevm <maksim.fedoseev122@gmail.com>
2024-10-23 16:47:53 +0200

    Modify zadanie4.py

--- 06. Przetwarzanie plików i operacje na napisach/zadanie4.py
+++ 06. Przetwarzanie plików i operacje na napisach/zadanie4.py
@@ -1,7 +1,13 @@
 with open("ciagi.txt", "r") as plik:
     liczba_ciagow = int(plik.readline().rstrip())
     ilosc_ciagow_parz = 0
-    for i in range(liczba_ciagow - 1):
+    for i in range(liczba_ciagow):
         linia = plik.readline().rstrip().split()
-        if sum(linia[1:]) % 2 == 0:
-            liczba_ciagow += 1
+        # print(linia[1:])
+        suma_elementow = sum(map(int, linia[1:]))
+        # print(suma_elementow)
+        # print(suma_elementow % 2)
+        if suma_elementow % 2 == 0:
+            ilosc_ciagow_parz += 1
+            # print(ilosc_ciagow_parz)
+print("Liczba ciagow z parzysta suma elementow: " + str(ilosc_ciagow_parz))
