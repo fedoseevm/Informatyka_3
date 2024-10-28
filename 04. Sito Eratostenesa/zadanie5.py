7325cbb2eca52cab5acdaf6ac0050ac051e62c83
fedoseevm <maksim.fedoseev122@gmail.com>
2024-10-28 19:57:56 +0100

    Modify zadanie5.py

--- 04. Sito Eratostenesa/zadanie5.py
+++ 04. Sito Eratostenesa/zadanie5.py
@@ -1,17 +1,16 @@
 a, b = map(int, input("Podaj przedzial (a i b): ").split())
 
-czy_pierwsza = [True] * (b - a + 1)
-print(czy_pierwsza)
+czy_pierwsza = [True] * (b + 1)
 czy_pierwsza[0] = False
 czy_pierwsza[1] = False
 
 p = 2
-while p * p < b:
+while p * p <= len(czy_pierwsza):
     if czy_pierwsza[p]:
         for i in range(p * p, len(czy_pierwsza), p):
             czy_pierwsza[i] = False
     p += 1
 
-for i in range(len(czy_pierwsza)):
+for i in range(a, b + 1):
     if czy_pierwsza[i]:
-        print(i + a, end=" ")
+        print(i, end=" ")
