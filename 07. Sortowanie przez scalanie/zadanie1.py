3135638065dcfa95269f9563382f285f78d790f8
fedoseevm <maksim.fedoseev122@gmail.com>
2024-10-30 13:17:40 +0100

    Add 07. Sortowanie przez scalanie

---
+++ 07. Sortowanie przez scalanie/zadanie1.py
@@ -0,0 +1,36 @@
+def Scale(T, left, center, right):
+    i = left
+    j = center + 1
+    k = left
+    while i <= center and j <= right:
+        if T[i] < T[j]:
+            tempTable[k] = T[i]
+            i += 1
+        else:
+            tempTable[k] = T[j]
+            j += 1
+        k += 1
+    while i <= center:
+        tempTable[k] = T[i]
+        i += 1
+        k += 1
+    while j <= right:
+        tempTable[k] = T[j]
+        j += 1
+        k += 1
+    for i in range(left, right):
+        T[i] = tempTable[i]
+
+
+def Sort(T, left, right):
+    if left < right:
+        center = (left + right) // 2
+        Sort(T, left, center)
+        Sort(T, center + 1, right)
+        Scale(T, left, center, right)
+
+
+ciag = list(map(int, input().split()))
+# print(ciag)
+tempTable = [0] * len(ciag)
+Sort(ciag, 0, len(ciag) - 1)
