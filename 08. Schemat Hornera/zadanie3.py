141597d47d43998ec9372fb58ef8c5763b8dc263
fedoseevm <maksim.fedoseev122@gmail.com>
2024-11-27 13:18:05 +0100

    Modify zadanie3.py

--- 08. Schemat Hornera/zadanie3.py
+++ 08. Schemat Hornera/zadanie3.py
@@ -1,1 +1,9 @@
 def HornerRecu(x, a):
+    if len(a) == 1:
+        return a[0]
+    return x * HornerRecu(x, a[1:]) + a[0]
+
+A = list(map(int, input("Podaj współczynniki wielomianu jako liczby rozdzielone spacjami: ").split()))
+x = int(input("Podaj x: "))
+
+print(f"Wartość wielomianu W({x}) = {HornerRecu(x, A)}")
