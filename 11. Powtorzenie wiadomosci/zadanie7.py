d847bab567dd31463bdfd0ffb1ba98dba7eb5d0c
fedoseevm <maksim.fedoseev122@gmail.com>
2025-03-09 22:16:34 +0100

    Modify zadanie 7

--- 11. Powtorzenie wiadomosci/zadanie7.py
+++ 11. Powtorzenie wiadomosci/zadanie7.py
@@ -4,4 +4,8 @@ with open("ciagi.txt", "r") as file:
         numbers = list(map(int, line.split()))
         print(numbers)
 
-n = len(numbers)
+        n = len(numbers)
+        for i in range(n - 2):
+            if sum(numbers[i:i + 2]) == 10:
+                print(numbers)
+                break
